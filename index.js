d12712e24748a563ec73e019ee03e037644bcb86
Lunakepio <moulinneufalex@gmail.com>
2025-03-18 12:14:48 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,69 @@
+import express from "express";
+const app = express();
+app.use(express.json());
+const port = 3000;
+
+const users = [
+  {
+    id: 0,
+    name: "Alex",
+  },
+  {
+    id: 1,
+    name: "Emma",
+  },
+];
+
+
+const sendError = (res,error) => {
+  res.send({ "error": error });
+}
+
+// default route
+app.get("/", (req, res) => {
+  res.send("Le serveur est bien démarré.");
+})
+
+// Get all users
+app.get("/users", (req, res) => {
+  res.json(users);
+  console.log('user requested all users');
+});
+
+// Get a single user
+app.get("/users/:id", (req, res) => {
+  const user = users.find(user => user.id === parseInt(req.params.id));
+  
+  if(!user){
+    res.status(404);
+    sendError(res, "User not found");
+  }
+  
+  res.json(user);
+})
+
+// Update a single user
+app.put("/users/:id", (req, res) => {
+  const user = users.find(user => user.id === parseInt(req.params.id));
+
+  if (!user) {
+    res.status(404);
+    return sendError(res, "User not found");
+  }
+
+  if (req.body.name) {
+    user.name = req.body.name
+  }
+
+  res.json(user);
+});
+
+app.delete("/users/:id", (req,res) => {
+  users = users.filter(user => user.id === parseInt(req.params.id))
+  res.json({ message: "Utilisateur supprimé" });
+})
+
+// Launch the server, start listening to events
+app.listen(port, () => {
+  console.log(`port démarré sur http://localhost:${port}`);
+});
