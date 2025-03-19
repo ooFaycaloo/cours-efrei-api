55d1f0b2e4bbaabf777cf4331acc122638b14f36
Lunakepio <moulinneufalex@gmail.com>
2025-03-19 11:03:34 +0100

    feat : added crypted password to user DB

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 import express from "express";
+import bcrypt from "bcrypt";
+import { findUser, sendError } from "./lib/index.js";
+
 const app = express();
+
 app.use(express.json());
+
 const port = 3000;
 
 let users = [
   {
     id: crypto.randomUUID(),
     name: "Alex",
+    password: bcrypt.hashSync("password123", 10)
   },
   {
     id: crypto.randomUUID(),
     name: "Emma",
+    password: bcrypt.hashSync("password456", 10)
   },
 ];
 
-
-const sendError = (res,error) => {
-  res.send({ "error": error });
-}
 
 // default route
 app.get("/", (req, res) => {
@@ -43,35 +46,81 @@ app.get("/users/:id", (req, res) => {
 })
 
 // Create a new user
-app.post("/users/", (req, res) => {
-  const newUser = {};
-  newUser.id = crypto.randomUUID();
-  if(typeof(req.body.name) === "string"){
-    newUser.name = req.body.name;
-    users.push(newUser);
-    res.status(201).json(newUser);
-  } else {
-    res.status(400).json("incorrect body parameters");
+app.post("/users/", async (req, res) => {
+  const { name, password } = req.body;
+  
+  if(!name || !password){
+    return sendError(res, "Name and password are required");
   }
   
+  if(findUser(users, name)){
+    return sendError(res, "User already exists");
+  }
   
+  const hashedPassword = await bcrypt.hash(password, 10);
+  
+  const newUser = {
+    id: crypto.randomUUID(),
+    name: name,
+    password: hashedPassword
+  };
+  
+  users.push(newUser);
+  res.status(201).json({ message: "user created successfully", newUser: newUser });
 })
 
+app.post("/login", (req, res) => {
+  const { name, password } = req.body;
+  
+  if(!name || !password){
+    return sendError(res, "Name and password are required");
+  }
+  
+  const user = findUser(users, name)
+  
+  if(!user){
+    return sendError(res, "User doesn't exist");
+  }
+  
+  const isMatch = bcrypt.compareSync(password, user.password);
+
+  
+  if(!isMatch){
+    sendError(res, "Name or Password doesn't match");
+  } else {
+    res.status(200).send("user logged in successfully");
+  }
+})
 
 // Update a single user
 app.put("/users/:id", (req, res) => {
-  const user = users.find(user => user.id === parseInt(req.params.id));
-
-  if (!user) {
-    res.status(404);
-    return sendError(res, "User not found");
+  const { name, oldPassword, newPassword } = req.body;
+  
+  if(!name || !newPassword){
+    return sendError(res, "Name and password are required");
   }
-
-  if (req.body.name) {
-    user.name = req.body.name
+  
+  const user = findUser(users, name)
+  
+  if(!user){
+    return sendError(res, "User doesn't exist");
   }
+  
+  const isMatch = bcrypt.compareSync(oldPassword, user.password);
+  
 
-  res.json(user);
+
+  if(isMatch){
+    user.name = name;
+    if(newPassword){
+      user.password = bcrypt.hashSync(newPassword, 10);
+    }
+    
+    res.status(200).json(user)
+  } else {
+    sendError(res, "Old password doesn't match");
+  }
+  
 });
 
 // Delete a user 
