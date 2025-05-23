0e8cf1a5d4d79b1987e85c2ac098d4f458666a9a
Lunakepio <moulinneufalex@gmail.com>
2025-05-23 10:45:54 +0200

    prepared data

--- index.test.js
+++ index.test.js
@@ -1,17 +1,21 @@
 import request from 'supertest';
 import app from './index.js';
+import mongoose from 'mongoose';
 
+beforeAll(async () => {
+  await mongoose.connect('mongodb://localhost:27017/my-database', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+});
 describe('Express App', () => {
   it('GET / should return Hello World', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
     expect(res.text).toBe('Hello World!');
   });
-
-  // it('POST /echo should return posted JSON', async () => {
-  //   const data = { name: 'Alice' };
-  //   const res = await request(app).post('/echo').send(data);
-  //   expect(res.statusCode).toBe(200);
-  //   expect(res.body).toEqual({ youSent: data });
-  // });
 });
